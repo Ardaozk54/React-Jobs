9b046d8e2413995246f94b0b08621b5f4075c30d
Arda Özkan <ardaozkan_54@hotmail.com>
2026-06-29 21:54:36 +0300

    Initial Commit - Components - Layouts

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './componenets/Navbar'
+import Hero from './componenets/Hero'
+import HomeCards from './componenets/HomeCards'
+import JobListings from './componenets/JobListings'
+import ViewAllJobs from './componenets/ViewAllJobs'
+
+const App = () => {
+  return (
+<>
+
+<Navbar/>
+<Hero/>
+<HomeCards/>
+<JobListings/> 
+<ViewAllJobs/>
+  
+ 
+
+  
+
+
+
+</>  )
+}
+
+export default App  ;
